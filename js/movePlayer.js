fe18a22f55284b78ae09d349a575a07ff8dd5dda
Binup karki <karki011@gmail.com>
2019-09-26 00:03:11 -0500

    got arrow workin

--- js/movePlayer.js
+++ js/movePlayer.js
@@ -8,14 +8,17 @@ function moveToNextCell(player, destination) {
 
 function movePlayer(event) {
     let player = document.querySelector('[data-type="S"]');
-    console.log('player:', player);
+    // console.log('player:', player);
     let currColumn = Number(player.dataset.cell);
-    console.log('currColumn:', currColumn)
+    // console.log('currColumn:', currColumn)
     let currRow = Number(player.dataset.row);
-    console.log('currRow:', currRow)
+    // console.log('currRow:', currRow)
     let destination;
+    
     destination = moveRight(event, destination, currRow, currColumn, player);
     destination = moveLeft(event, destination, currRow, currColumn, player);
+    destination = moveUp(event, destination, currRow, currColumn, player);
+    destination = moveDown(event, destination, currRow, currColumn, player);
 
 
 }
@@ -25,7 +28,7 @@ function moveRight(event, destination, currRow, currColumn, player) {
         if (event.key === 'ArrowRight') {
             destination = document.getElementById(currRow + "-" + (currColumn + 1));
             console.log('destination:', destination);
-            if (destination.dataset != "W") {
+            if (destination.dataset.type !== "W") {
                 console.log("NON");
                 moveToNextCell(player, destination);
                 playerLocation.cell++;
@@ -39,8 +42,32 @@ function moveLeft(event, destination, currRow, currColumn, player) {
         if (event.key === 'ArrowLeft') {
             destination = document.getElementById(currRow + "-" + (currColumn - 1));
             console.log('destination:', destination);
-            if (destination.dataset != "W") {
+            if (destination.dataset.type !== "W") {
                 console.log("NON");
+                moveToNextCell(player, destination);
+                playerLocation.cell--;
+            }
+        }
+    }
+    return destination;
+}
+function moveUp(event, destination, currRow, currColumn, player) {
+    if (gameActive) {
+        if (event.key === 'ArrowUp') {
+            destination = document.getElementById(currRow -1+ "-" + (currColumn));
+            if (destination.dataset.type !== "W") {
+                moveToNextCell(player, destination);
+                playerLocation.cell--;
+            }
+        }
+    }
+    return destination;
+}
+function moveDown(event, destination, currRow, currColumn, player) {
+    if (gameActive) {
+        if (event.key === 'ArrowDown') {
+            destination = document.getElementById(currRow +1+ "-" + (currColumn));
+            if (destination.dataset.type !== "W") {
                 moveToNextCell(player, destination);
                 playerLocation.cell--;
             }
